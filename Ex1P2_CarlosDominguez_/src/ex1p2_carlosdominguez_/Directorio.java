9f6fbe01d9d44eeffd327e0e99f5ccb583bc0883
cadodominguez <itzkarurosu@gmail.com>
2024-08-16 17:05:15 -0600

    ya termine

--- Ex1P2_CarlosDominguez_/src/ex1p2_carlosdominguez_/Directorio.java
+++ Ex1P2_CarlosDominguez_/src/ex1p2_carlosdominguez_/Directorio.java
@@ -1,33 +1,13 @@
 
 package ex1p2_carlosdominguez_;
-import java.util.Random;
+
 public class Directorio extends Creacion {
-    private String codigo = cod();
-    static Random rd = new Random();
-    
     public Directorio(String nombre) {
         super(nombre);
     }
-    
+
     @Override
     public String visualisacion() {
-        return codigo;
+        return "A sucedido un error!";
     }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-    public String cod(){
-        String cods = "";
-        for (int i = 0; i <= 20; i++) {
-            int random = rd.nextInt(65, 90);
-            cods += (char) random;
-        }
-        return cods;
-    }
-    
 }
